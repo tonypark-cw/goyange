50ac9dbb5f0d49e3c5afe2bbd0bd8697282dbf83
unknown <qkrcksdna@naver.com>
2021-02-03 13:43:12 +0900

    모달창 닫기 기능 추가

--- src/ImageInfo.js
+++ src/ImageInfo.js
@@ -7,7 +7,6 @@ class ImageInfo {
       $imageInfo.className = "ImageInfo";
       this.$imageInfo = $imageInfo;
       $target.appendChild($imageInfo);
-  
       this.data = data;
   
       this.render();
@@ -19,25 +18,50 @@ class ImageInfo {
     }
   
     render() {
-      if (this.data.visible) {
-        const { name, url, temperament, origin } = this.data.image;
+      
   
+      if (this.data.visible) {
+        
+        const { name, url, temperament, origin } = this.data.image;
         this.$imageInfo.innerHTML = `
           <div class="content-wrapper">
             <div class="title">
               <span>${name}</span>
-              <div class="close">x</div>
+              <div id="closeBtn">x</div>
             </div>
             <img src="${url}" alt="${name}"/>        
             <div class="description">
               <div>성격: ${temperament}</div>
               <div>태생: ${origin}</div>
             </div>
-          </div>`;
+          </div>
+          <script>
+        `;
         this.$imageInfo.style.display = "block";
       } else {
         this.$imageInfo.style.display = "none";
       }
+
+      let close = document.getElementById("closeBtn");
+      if(close){
+        close.addEventListener("click", function (e){
+          document.querySelector(".ImageInfo").style.display="none";
+        });
+      }
+      
+      window.onkeydown = function(e){
+        if(e.keyCode == 27){
+          document.querySelector(".ImageInfo").style.display="none";
+        }
+      }
+
+      window.addEventListener("click", function(e){
+        if(e.target.className == "ImageInfo"){
+          document.querySelector(".ImageInfo").style.display="none";
+        }
+      });
+
     }
+    
   }
   
