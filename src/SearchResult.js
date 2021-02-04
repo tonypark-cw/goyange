be87cb6283561b5ef89821c5dec09f9af4653515
unknown <qkrcksdna@naver.com>
2021-02-04 17:45:33 +0900

    스크롤페이징 기능만 가능. 추후 개선 필요

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -13,34 +13,54 @@ class SearchResult {
   
       this.render();
     }
-  
+
     setState(nextData) {
       this.data = nextData;
       this.render();
     }
-  
+
     render() {
-      
-      this.$searchResult.innerHTML = this.data
-      .map(
+      // inner HTML에 넣는걸 한번 거쳐서 하도록 수정해야함.
+      // 그래야 페이지로 나눠서 넣고, scroll paging 이 가능해짐.
+      let temp = this.data.map(
         cat => `
           <div class="item">
-            <img class="lazy" data-src=${cat.url} alt=${cat.name} src="./public/gray.png" loading="lazy" width="400" height="500" />
+            <img class="lazy" src=${cat.url} alt=${cat.name} loading="lazy" width="400" height="500" />
           </div>
         `
-      )
-      .join("");
-    
+      );
+
+      console.log(temp);
+      let idx = 12;
+      let partial = temp.slice(0, idx).join("");
+      this.$searchResult.innerHTML = partial;
+      let count = 0;
+      //스크롤 바닥 감지
+      window.onscroll = function(e) {
+        
+          //추가되는 임시 콘텐츠
+          //window height + window scrollY 값이 document height보다 클 경우,
+          if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            //실행할 로직 (콘텐츠 추가)
+              count++;
+              console.log(e.target);
+              let dest = document.querySelector(".SearchResult");
+              dest.innerHTML += temp.slice(idx, idx+3).join("");
+              idx += 4;
+              
+          }
+      
+      };
+
       const images = document.querySelectorAll("img");
       const lazyLoad = (target) => {
         const io = new IntersectionObserver((entries, observer) => {
           entries.forEach((entry) => {
-            // console.log("reached!");
             if (entry.isIntersecting) {
               const img = entry.target; // 이미지 엘리멘트를 가져옵니다.
-              const src = img.getAttribute("data-src");
+              const src = img.getAttribute("src");
               img.setAttribute("src", src);
-              img.classList.add("fade");
+              // img.classList.add("fade");
               observer.disconnect();
             }
           });
@@ -53,15 +73,21 @@ class SearchResult {
           this.onClick(this.data[index]);
         });
       });
-      try{ 
-        if(this.data.length <= 0){
-          this.$searchResult.innerHTML = `
-          <div>검색결과가 없습니다.</div>
-          `;
-        }
-      }catch{
-        console.log(this.data);
-      }
       images.forEach(lazyLoad);
+      if(this.data.length <= 0){
+        this.$searchResult.innerHTML = `
+        <div>검색결과가 없습니다.</div>
+        `;
+      }
+      
+      // window.onmousewheel = function(e){
+      //   console.log(e);
+      //   console.log(e.pageX);
+      //   if(e.wheelDelta === -120){
+          
+      //   }else{
+      //     console.log("휠 업");
+      //   }
+      // }
     }
-  }
+}
